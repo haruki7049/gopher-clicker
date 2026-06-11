dc1428caaf9a51c6b8cef7992ee8ea9ce3b81f32
haruki7049 <tontonkirikiri@gmail.com>
2026-06-11 19:43:31 +0900

    Update

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	// Standard libraries
+	"bytes"
 	"image/color"
 	"log"
 
+	// Externals
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
+	"golang.org/x/image/font/gofont/goregular"
 
+	// Internals
 	"github.com/haruki7049/gopher-clicker/assets"
 )
+
+const GAME_HEIGHT = 480
+const GAME_WIDTH = 640
 
 func gopherColor() color.RGBA {
 	return color.RGBA{R: 0x00, G: 0xad, B: 0xd8, A: 0xff}
@@ -23,9 +32,10 @@ type Gopher struct {
 }
 
 type Game struct {
-	isTitle bool
-	ticks   int
-	gopher  Gopher
+	isTitle  bool
+	ticks    int
+	gopher   Gopher
+	fontFace *text.GoTextFaceSource
 }
 
 func newGame() (*Game, error) {
@@ -39,8 +49,15 @@ func newGame() (*Game, error) {
 	g.gopher.image = gopher_img
 
 	// Set initial gopher scale
-	g.gopher.scaleX = 0.5
-	g.gopher.scaleY = 0.5
+	g.gopher.scaleX = 1.0
+	g.gopher.scaleY = 1.0
+
+	// Set the standard Go font TTF data
+	s, err := text.NewGoTextFaceSource(bytes.NewReader(goregular.TTF))
+	if err != nil {
+		log.Fatal(err)
+	}
+	g.fontFace = s
 
 	return g, nil
 }
@@ -60,17 +77,32 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(gopherColor())
 
 	// Draw Gopher image
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Scale(g.gopher.scaleX, g.gopher.scaleY)
-	screen.DrawImage(g.gopher.image, op)
+	{
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Scale(g.gopher.scaleX, g.gopher.scaleY)
+		screen.DrawImage(g.gopher.image, op)
+	}
+
+	// Draw title
+	{
+		face := &text.GoTextFace{
+			Source: g.fontFace,
+			Size:   24,
+		}
+		op := &text.DrawOptions{}
+		op.GeoM.Translate(GAME_WIDTH/2, GAME_HEIGHT/3*2)
+
+		// Draw
+		text.Draw(screen, "Gopher Clicker", face, op)
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return GAME_WIDTH, GAME_HEIGHT
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowSize(GAME_WIDTH, GAME_HEIGHT)
 	ebiten.SetWindowTitle("Hello, World!")
 	g, err := newGame()
 	if err != nil {
