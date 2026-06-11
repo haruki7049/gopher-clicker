062f04079c792c3ffc5ecff7dbef411869be6425
haruki7049 <tontonkirikiri@gmail.com>
2026-06-11 18:38:37 +0900

    Update

--- main.go
+++ main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Game struct {
+	ticks int
+}
 
 func (g *Game) Update() error {
+	// Increment ticks and reset at 120 to prevent overflow
+	g.ticks++
+	if g.ticks >= 120 {
+		g.ticks = 0
+	}
+
 	return nil
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+func (game *Game) Draw(screen *ebiten.Image) {
+	// Determine the current step in the 120-tick cycle
+	cycle := game.ticks
+	var r uint8
+	var g uint8
+	var b uint8
+
+	if cycle < 60 {
+		// Fade from black to white (0 to 255)
+		r = uint8((cycle * 255) / 60)
+		g = uint8((cycle * 255) / 30)
+		b = uint8((cycle * 255) / 20)
+	} else {
+		// Fade from white to black (255 to 0)
+		r = uint8(((120 - cycle) * 255) / 60)
+		g = uint8(((120 - cycle) * 255) / 30)
+		b = uint8(((120 - cycle) * 255) / 20)
+	}
+
+	// Fill the screen with the calculated color
+	screen.Fill(color.RGBA{R: r, G: g, B: b, A: 255})
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
