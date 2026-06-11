21e10d5a56dbe94346cb01db60d3f8e66c583ace
haruki7049 <tontonkirikiri@gmail.com>
2026-06-11 18:40:48 +0900

    Update

--- main.go
+++ main.go
@@ -22,26 +22,8 @@ func (g *Game) Update() error {
 }
 
 func (game *Game) Draw(screen *ebiten.Image) {
-	// Determine the current step in the 120-tick cycle
-	cycle := game.ticks
-	var r uint8
-	var g uint8
-	var b uint8
-
-	if cycle < 60 {
-		// Fade from black to white (0 to 255)
-		r = uint8((cycle * 255) / 60)
-		g = uint8((cycle * 255) / 30)
-		b = uint8((cycle * 255) / 20)
-	} else {
-		// Fade from white to black (255 to 0)
-		r = uint8(((120 - cycle) * 255) / 60)
-		g = uint8(((120 - cycle) * 255) / 30)
-		b = uint8(((120 - cycle) * 255) / 20)
-	}
-
 	// Fill the screen with the calculated color
-	screen.Fill(color.RGBA{R: r, G: g, B: b, A: 255})
+	screen.Fill(color.RGBA{R: 0x00, G: 0xad, B: 0xd8, A: 0xff})
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
