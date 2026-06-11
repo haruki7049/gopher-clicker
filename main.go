e4f4d0291e34f532cc4b7c81d9f077b998d37c3d
haruki7049 <tontonkirikiri@gmail.com>
2026-06-11 19:00:47 +0900

    Update

--- main.go
+++ main.go
@@ -5,10 +5,33 @@ import (
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+
+	"github.com/haruki7049/gopher-clicker/assets"
 )
 
+type Gopher struct {
+	image *ebiten.Image
+	x     int
+	y     int
+}
+
 type Game struct {
-	ticks int
+	ticks  int
+	gopher Gopher
+}
+
+func newGame() (*Game, error) {
+	g := &Game{}
+
+	// Load gopher image
+	gopher_img, _, err := ebitenutil.NewImageFromFileSystem(assets.Assets, "images/gopher.png")
+	if err != nil {
+		return nil, err
+	}
+	g.gopher.image = gopher_img
+
+	return g, nil
 }
 
 func (g *Game) Update() error {
@@ -21,9 +44,13 @@ func (g *Game) Update() error {
 	return nil
 }
 
-func (game *Game) Draw(screen *ebiten.Image) {
+func (g *Game) Draw(screen *ebiten.Image) {
 	// Fill the screen with the calculated color
 	screen.Fill(color.RGBA{R: 0x00, G: 0xad, B: 0xd8, A: 0xff})
+
+	// Draw Gopher image
+	op := &ebiten.DrawImageOptions{}
+	screen.DrawImage(g.gopher.image, op)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -33,7 +60,12 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	g, err := newGame()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
 }
